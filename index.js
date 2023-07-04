6e66a27ad4418e6dd7cfc4b0e28f72b3e49b8fd2
Maryam <maryamjaved.xo@gmail.com>
2023-07-04 22:44:07 +0500

    shooting projectiles

--- index.js
+++ index.js
@@ -49,6 +49,33 @@ class Player {
     
 }
 
+
+// --------------------------------projectile class---------------------------------------------------
+
+class Projectile {
+    constructor({ position, velocity }) {
+      this.position = position
+      this.velocity = velocity
+      this.radius = 5
+    }
+  
+    draw() {
+      c.beginPath()
+      c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false)
+      c.closePath()
+      c.fillStyle = 'white'
+      c.fill()
+    }
+  
+    update() {
+      this.draw()
+      this.position.x += this.velocity.x
+      this.position.y += this.velocity.y
+    }
+  }
+
+// ---------------------------------------------------------------------------------------------------  
+
 const player = new Player({
     position: { x: canvas.width / 2, y: canvas.height / 2 },
     velocity: { x: 0, y: 0 },
@@ -72,12 +99,34 @@ const player = new Player({
   const SPEED=3;
   const ROTATIONAL_SPEED=0.05;
   const FRICTION=0.97;
+  const PROJECTILE_SPEED=3;
+
+  const projectiles = []
+  const asteroids = []
+
 
   function animate() {
     const animationId = window.requestAnimationFrame(animate)  //we are calling animate again and again
     c.fillStyle='black'
     c.fillRect(0,0, canvas.width, canvas.height); //first two args are position other two are width and height
     player.update();
+
+    
+    for (let i = projectiles.length - 1; i >= 0; i--) {
+        const projectile = projectiles[i]
+        projectile.update()
+
+        if (
+            projectile.position.x + projectile.radius < 0 ||
+            projectile.position.x - projectile.radius > canvas.width ||
+            projectile.position.y - projectile.radius > canvas.height ||
+            projectile.position.y + projectile.radius < 0
+          ) {
+            projectiles.splice(i, 1)
+          }
+    }
+
+
     if (keys.w.pressed) {
         player.velocity.x = Math.cos(player.rotation)*SPEED
         player.velocity.y = Math.sin(player.rotation)*SPEED
@@ -106,6 +155,23 @@ const player = new Player({
       case 'KeyD':
         keys.d.pressed = true
         break
+        case 'Space':
+            projectiles.push(
+              new Projectile({
+                position: {
+                  x: player.position.x + Math.cos(player.rotation) * 30,
+                  y: player.position.y + Math.sin(player.rotation) * 30,
+                },
+
+                velocity: {
+                    x: Math.cos(player.rotation) * PROJECTILE_SPEED,
+                    y: Math.sin(player.rotation) * PROJECTILE_SPEED,
+                  },
+                })
+              )
+        
+              break
+
     }
     })
 
